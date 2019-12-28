5976fc9b51806db6cddc13f0f1e3f3b121de183a
Shawn Loh <shawnlohb@gmail.com>
2019-12-28 16:52:49 +0800

    Added requiring models dynamically for models

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
 const envConfigs = require('./config/config');
 
 // db seed
-const seed = require('./seeds/seed');
+// const seed = require('./seeds/seed');
+
 // models
-const account = require('../models/account');
-const status = require('../models/status');
-const rank = require('../models/rank');
-const platoon = require('../models/platoon');
-const point = require('../models/point');
+// const account = require('../models/account');
+// const event = require('../models/event');
+// const person = require('../models/person');
+// const personnelPoint = require('../models/personnelPoint');
+// const personnelStatus = require('../models/personnelStatus');
+// const platoon = require('../models/platoon');
+// const point = require('../models/point');
+// const rank = require('../models/rank');
+// const status = require('../models/status');
 
 // Change mongoose promise to use bluebird instead
 mongoose.Promise = Promise;
@@ -24,26 +31,38 @@ const env = process.env.NODE_ENV || 'development';
 const config = envConfigs[env];
 mongoose.connect(config.url);
 
+const dbModels ={};
+const loadDbModels = (pathName) => {
+  const stat =fs.lstatSync(pathName);
+  if (stat.isDirectory()) {
+    const files = fs.readdirSync(pathName);
+    let f; const l = files.length;
+    for (let i = 0; i < l; i++) {
+      f = path.join(pathName, files[i]);
+      dbModels[`${i}`] = require(f);
+    }
+  } else {
+    dbModels['0'] = require(pathName);
+  }
+};
 
 // Init database index for unique
 const setupModelsIndex = async () => {
-  await Promise.all([
-    account.init(),
-    status.init(),
-    rank.init(),
-    platoon.init(),
-    point.init(),
-  ]).then(() => {
-    // console.log('finished setting up database index');
-  }).catch((error)=>{
-    console.log('error setting up database index');
-  });
+  try {
+    loadDbModels(path.join(__dirname, '..', 'models'));
+    await Promise.all(Object.keys(dbModels).map((key) => {
+      return dbModels[key].init();
+    }));
 
-  // Seed database is env is development
-  if (env == 'development') {
+    console.log('Finished models indexing');
+    if (env == 'development') {
     // await seed.clearDB();
     // await seed.all();
-  };
+    };
+  } catch (error) {
+    console.error(error);
+    console.log('Error setting up database index');
+  }
 };
 
 // initiate set up models indexing
